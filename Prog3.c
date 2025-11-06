8433b2bdc880710b88144241b856ff09bcd5d840
desi-hw <desireewasszabo@gmail.com>
2025-11-05 23:40:12 -0600

    Add Prog3.c to print numbers from 100 to 1

---
+++ Prog3.c
@@ -0,0 +1,11 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Ejercicio 3 Estructurasa de Control Repetitivas*/
+#include<stdio.h>
+void main()
+{
+  int num;
+  for(num=100; num>0; num--)
+  {
+    printf("%d \n", num);
+  }
+}
