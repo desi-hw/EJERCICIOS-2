1b119f48d26b9f77fa588e699e52ae079433a10b
desi-hw <desireewasszabo@gmail.com>
2025-11-05 23:14:37 -0600

    Add Prog1.c with a loop to print numbers 1-100

---
+++ Prog1.c
@@ -0,0 +1,11 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Ejercicio 1 Estructuras de Control Repetitivas*/
+#include<stdio.h>
+void main()
+{
+  int num;
+  for(num=1; num>0 && num<101; num++)
+  {
+    printf("%d \n", num);
+  }
+}
