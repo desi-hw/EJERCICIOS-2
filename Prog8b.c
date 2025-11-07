63c887e20e222c668ce2766ed5eb08992ddc7c54
desi-hw <desireewasszabo@gmail.com>
2025-11-07 13:48:07 -0600

    Implement power calculation using loops

--- Prog8b.c
+++ Prog8b.c
@@ -3,5 +3,14 @@ Ejercicio 8b Estructuras de Control Repetitivas*/
 #include<stdio.h>
 void main()
 {
-  
+  int num, pot, res=1, i;
+  printf("Dame un numero: ");
+  scanf("%d", &num);
+  printf("Dame un valor para la potencia: ");
+  scanf("%d", &pot);
+  for(i = 1; i <= pot; i++) 
+  {
+    res= res*num;
+  }
+  printf("El resultado es: %d", res);
 }
